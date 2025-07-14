6d8192c7326f47d1414d2fe400270208707e3c6f
Alejandro Barrera <alejandro91dark@hotmail.es>
2025-07-14 15:42:14 -0400

    homework

--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
-import { ProductCardComponent } from '../../../products/components/product-card/product-card.component';
+import { ProductsService } from './../../../products/services/products.service';
+import { Component, inject } from '@angular/core';
+import { ProductCardComponent } from '@products/components/product-card/product-card.component';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-home-page',
   imports: [ProductCardComponent],
   templateUrl: './home-page.component.html',
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  private productsService = inject(ProductsService);
+
+  public productsResource = rxResource({
+    request: () => ({}),
+    loader: ({ request }) => {
+      return this.productsService.getProducts({});
+    },
+  });
+}
