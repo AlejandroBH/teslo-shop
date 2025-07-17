aabd01c66fcdd8ec4e472c25561f5326428a6276
Alejandro Barrera <alejandro91dark@hotmail.es>
2025-07-17 14:09:05 -0400

    homework

---
+++ src/app/admin-dashboard/admin-dashboard.routes.ts
@@ -0,0 +1,32 @@
+import { Routes } from '@angular/router';
+import { AdminDashboardLayoutComponent } from './layouts/admin-dashboard-layout/admin-dashboard-layout.component';
+import { ProductAdminPageComponent } from './pages/product-admin-page/product-admin-page.component';
+import { ProductsAdminPageComponent } from './pages/products-admin-page/products-admin-page.component';
+import { ProductDashboardPageComponent } from './pages/product-dashboard-page/product-dashboard-page.component';
+
+export const adminDashboardRoutes: Routes = [
+  {
+    path: '',
+    component: AdminDashboardLayoutComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: ProductDashboardPageComponent,
+      },
+      {
+        path: 'products',
+        component: ProductsAdminPageComponent,
+      },
+      {
+        path: 'product/:id',
+        component: ProductAdminPageComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'products',
+      },
+    ],
+  },
+];
+
+export default adminDashboardRoutes;
---
+++ src/app/admin-dashboard/pages/product-dashboard-page/product-dashboard-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-dashboard-page',
+  imports: [],
+  templateUrl: './product-dashboard-page.component.html',
+})
+export class ProductDashboardPageComponent { }
