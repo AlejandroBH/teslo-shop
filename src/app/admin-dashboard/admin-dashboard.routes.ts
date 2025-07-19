1b42b5254e87af74b6c0c267afa6873b8d5f6a04
Alejandro Barrera <alejandro91dark@hotmail.es>
2025-07-19 16:59:18 -0400

    admin product page and pagination

--- src/app/admin-dashboard/admin-dashboard.routes.ts
+++ src/app/admin-dashboard/admin-dashboard.routes.ts
@@ -20,7 +20,7 @@ export const adminDashboardRoutes: Routes = [
         component: ProductsAdminPageComponent,
       },
       {
-        path: 'product/:id',
+        path: 'products/:id',
         component: ProductAdminPageComponent,
       },
       {
--- src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
+++ src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
 import { ProductTableComponent } from '@products/components/product-table/product-table.component';
+import { ProductsService } from '@products/services/products.service';
+import { PaginationComponent } from '@shared/components/pagination/pagination.component';
+import { PaginationService } from '@shared/components/pagination/pagination.service';
 
 @Component({
   selector: 'app-products-admin-page',
-  imports: [ProductTableComponent],
+  imports: [ProductTableComponent, PaginationComponent],
   templateUrl: './products-admin-page.component.html',
 })
-export class ProductsAdminPageComponent {}
+export class ProductsAdminPageComponent {
+  private productsService = inject(ProductsService);
+  public paginationService = inject(PaginationService);
+  public productsPerPage = signal(10);
+
+  public productsResource = rxResource({
+    request: () => ({
+      page: this.paginationService.currentPage() - 1,
+      limit: this.productsPerPage(),
+    }),
+    loader: ({ request }) => {
+      return this.productsService.getProducts({
+        offset: request.page * 9,
+        limit: request.limit,
+      });
+    },
+  });
+}
