fbb05e88c921793d1813d721f7e39d4ac9eea37e
Alejandro Barrera <alejandro91dark@hotmail.es>
2025-07-19 20:30:33 -0400

    product dashboard details

---
+++ src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.component.ts
@@ -0,0 +1,49 @@
+import { Component, inject, input, OnInit } from '@angular/core';
+import { Product } from '@products/interfaces/product.interface';
+import { ProductCarouselComponent } from '@products/components/product-carousel/product-carousel.component';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '@utils/forms-utils';
+
+@Component({
+  selector: 'product-details',
+  imports: [ProductCarouselComponent, ReactiveFormsModule],
+  templateUrl: './product-details.component.html',
+})
+export class ProductDetailsComponent implements OnInit {
+  public product = input.required<Product>();
+  private fb = inject(FormBuilder);
+
+  public productForm = this.fb.group({
+    title: ['', Validators.required],
+    description: ['', Validators.required],
+    slug: [
+      '',
+      [Validators.required, Validators.pattern(FormUtils.slugPattern)],
+    ],
+    price: [0, [Validators.required, Validators.min(0)]],
+    stock: [0, [Validators.required, Validators.min(0)]],
+    sizes: [['']],
+    images: [[]],
+    tags: [''],
+    gender: [
+      'men',
+      [Validators.required, Validators.pattern(/men|women|kid|unisex/)],
+    ],
+  });
+
+  public sizes = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
+
+  public ngOnInit(): void {
+    this.setFormValue(this.product());
+  }
+
+  public setFormValue(formLike: Partial<Product>) {
+    this.productForm.reset(this.product() as any);
+    this.productForm.patchValue({ tags: formLike.tags?.join(',') });
+    // this.productForm.patchValue(formLike as any);
+  }
+
+  public onSubmit() {
+    console.log(this.productForm.value);
+  }
+}
